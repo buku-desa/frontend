8b5e4fc8e29aa29569940b9c51f8f350293395c5
marissssssa <marissan4fi@gmail.com>
2025-11-01 03:54:24 +0700

    Add warga frontend pages and components

---
+++ warga-fe-baru/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'Inter, system-ui, -apple-system, "Segoe UI", Roboto' }}>
+      <h1>Halo — proyek TypeScript (.tsx) siap!</h1>
+      <p>Ini file `src/App.tsx` contoh.</p>
+    </div>
+  );
+}
